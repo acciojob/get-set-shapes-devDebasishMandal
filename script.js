b262a1a79ac09bb2ce64530bcc025b1d588be01d
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-11 00:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-//complete this code
 class Rectangle {
-	construtor(width,height){
-		this._width=width;
-		this._height=height;
-	}
-	get height(){
-		return this._height;
-	}
-	get width (){
-		return this._width;
-	}
-	getArea(){
-		return (this._width)*(this._height);
-	}
-}
-class Square extends Rectangle {
-constructor(side){
-	super(side,side);
-	this._side=side;
-}
-	getPerimeter(){
-		return 4*(this._side);
+	constructor(width, height) {
+		this._width = width;
+		this._height = height;
 	}
 
-	
+	get width() {
+		return this._width;
+	}
+
+	get height() {
+		return this._height;
+	}
+
+	getArea() {
+		return this._width * this._height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		super(side, side);
+	}
+
+	getPerimeter() {
+		return 4 * this._width;
+	}
 }
 
 // Do not change the code below this line
